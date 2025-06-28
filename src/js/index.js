7e5d602e9a2e5b864fecfdbbf3e161d7e819e4d7
edoalvin <edoalvin123@gmail.com>
2025-06-28 14:21:16 +0700

    feat: vantajs

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,15 @@
-
+VANTA.WAVES({
+  el: "#vanta-bg",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x0,
+  shininess: 27.00,
+  waveHeight: 18.50,
+  waveSpeed: 0.35,
+  zoom: 1.42
+})
