a999ee2771d43f56b055d18149a23d7cbc7845e2
edoalvin <edoalvin123@gmail.com>
2025-06-30 01:26:53 +0700

    feat: navbar smooth scroll & hover on skills

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,30 @@
+//href untuk semua navbar ke setiap id (NAVBAR)
+document.addEventListener("DOMContentLoaded", () => {
+    // Smooth scroll untuk semua anchor yang menuju id (#)
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            const href = this.getAttribute('href');
+            if (href === "# ") {
+                // Kalau linknya hanya #, scroll ke atas
+                window.scrollTo({
+                    top: 0,
+                    behavior: 'smooth'
+                });
+            } else {
+                const target = document.querySelector(href);
+                if (target) {
+                    target.scrollIntoView({
+                        behavior: 'smooth'
+                    });
+                }
+            }
+        });
+    });
+});
+
 VANTA.NET({
-  el: "#vanta-bg",
+  el: "#hero",
   mouseControls: true,
   touchControls: true,
   gyroControls: false,
@@ -14,3 +39,4 @@ VANTA.NET({
   spacing: 18.00,
   showDots: true
 })
+
