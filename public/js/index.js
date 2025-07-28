70e1bc785a78f1f5cd4d0dcf02b711c756e6f21f
edoalvin <edoalvin123@gmail.com>
2025-07-28 20:51:57 +0700

    feat: contact start

--- public/js/index.js
+++ public/js/index.js
@@ -23,21 +23,5 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-VANTA.NET({
-  el: "#hero",
-  mouseControls: true,
-  touchControls: true,
-  gyroControls: false,
-  maxHeight: 100.00,
-  minWidth: 200.00,
-  scale: 1.00,
-  scaleMobile: 1.00,
-  color: 0x777777,
-  backgroundColor: 0x0,
-  points: 10.00,
-  maxDistance: 19.00,
-  spacing: 18.00,
-  showDots: true
-})
 
 
